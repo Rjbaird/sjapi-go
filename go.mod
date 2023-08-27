c51303d806e4b87e77eed417b0d8d76167d6554a
Rjbaird <rjbaird09@gmail.com>
2023-08-26 19:05:14 -0500

    add mongodb

--- go.mod
+++ go.mod
@@ -2,6 +2,8 @@ module github.com/bairrya/sjapi
 
 go 1.21
 
+require github.com/joho/godotenv v1.5.1
+
 require (
 	github.com/PuerkitoBio/goquery v1.8.1 // indirect
 	github.com/andybalholm/cascadia v1.3.2 // indirect
@@ -12,11 +14,20 @@ require (
 	github.com/gocolly/colly v1.2.0 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
-	github.com/joho/godotenv v1.5.1
+	github.com/golang/snappy v0.0.1 // indirect
 	github.com/kennygrant/sanitize v1.2.4 // indirect
+	github.com/klauspost/compress v1.13.6 // indirect
+	github.com/montanaflynn/stats v0.0.0-20171201202039-1bf9dbcd8cbe // indirect
 	github.com/saintfish/chardet v0.0.0-20230101081208-5e3ef4b5456d // indirect
 	github.com/temoto/robotstxt v1.1.2 // indirect
+	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
+	github.com/xdg-go/scram v1.1.2 // indirect
+	github.com/xdg-go/stringprep v1.0.4 // indirect
+	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
+	go.mongodb.org/mongo-driver v1.12.1 // indirect
+	golang.org/x/crypto v0.12.0 // indirect
 	golang.org/x/net v0.14.0 // indirect
+	golang.org/x/sync v0.1.0 // indirect
 	golang.org/x/text v0.12.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/protobuf v1.31.0 // indirect
--- server.go
+++ server.go
@@ -1,33 +1,58 @@
 package main
 
 import (
-	"encoding/json"
+	"context"
 	"fmt"
+	"log"
+	"os"
 	"strings"
 
 	"github.com/gocolly/colly"
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type RecentManga struct {
-	Title         string
-	Link          string
-	RecentChapter string
-	RecentLink    string
+	// ID            primitive.ObjectID `json:"id" bson:"_id,omitempts"`
+	Title         string `json:"title" bson:"title,omitempts"`
+	Link          string `json:"link" bson:"link,omitempts"`
+	RecentChapter string `json:"recent_chapters" bson:"recent_chapter,omitempts"`
+	RecentLink    string `json:"recent_link" bson:"recent_link,omitempts"`
 }
 
-const recentURL = "https://www.viz.com/read/shonenjump/section/free-chapters"
-
 func main() {
-	results := []RecentManga{}
-	c := colly.NewCollector()
-    c.OnRequest(func(r *colly.Request) {
-        // r.Headers.Set()
-        fmt.Println("Visiting", r.URL)
-    })
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found")
+	}
 
-    c.OnError(func(_ *colly.Response, err error) {
-        fmt.Println("Something went wrong:", err)
-    })
+	uri := os.Getenv("MONGODB_URI")
+	if uri == "" {
+		log.Fatal("You must set your 'MONGODB_URI' environment variable. See\n\t https://www.mongodb.com/docs/drivers/go/current/usage-examples/#environment-variable")
+	}
+
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
+	if err != nil {
+		panic(err)
+	}
+
+	db := client.Database("shonen-jump")
+	mangaCollection := db.Collection("manga")
+	defer client.Disconnect(context.Background())
+	const recentURL = "https://www.viz.com/read/shonenjump/section/free-chapters"
+
+	results := []RecentManga{}
+
+	c := colly.NewCollector()
+
+	c.OnRequest(func(r *colly.Request) {
+		// r.Headers.Set()
+		fmt.Println("Visiting", r.URL)
+	})
+
+	c.OnError(func(_ *colly.Response, err error) {
+		fmt.Println("Something went wrong:", err)
+	})
 
 	c.OnHTML(".o_sortable", func(e *colly.HTMLElement) {
 		recent := strings.Split(e.ChildText("span"), "  ")[0]
@@ -37,12 +62,25 @@ func main() {
 			RecentChapter: strings.Replace(recent, "Latest: ", "", -1),
 			RecentLink:    e.ChildAttr("a.o_inner-link", "href"),
 		}
+
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("%+v\n", manga)
 		results = append(results, manga)
 	})
 	c.Visit(recentURL)
-	data, err := json.MarshalIndent(results, " ", "")
-	if err != nil {
-		fmt.Println(err)
+
+	mangaDocs := []interface{}{}
+	for _, manga := range results {
+		mangaDocs = append(mangaDocs, manga)
 	}
-	fmt.Println(string(data))
+
+	// fmt.Printf("%+v\n", mangaDocs...)
+	result, err := mangaCollection.InsertMany(context.TODO(), mangaDocs)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(result)
 }
