8cc450fbe37aa6e1dec33df4775584e3bcb34241
Rjbaird <rjbaird09@gmail.com>
2023-08-27 18:47:25 -0500

    add series scrape

--- server.go
+++ server.go
@@ -1,39 +1,41 @@
 package main
 
 import (
-	"context"
 	"fmt"
 
-	"github.com/bairrya/sjapi/db"
 	"github.com/bairrya/sjapi/web"
 )
 
 func main() {
 
+	results, err := web.ScrapeSeries("naruto")
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(results)
+
 	// Connect to MongoDB
-	client, err := db.Init()
-	if err != nil {
-		panic(err)
-	}
-	defer client.Disconnect(context.Background())
+	// client, err := db.Init()
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// defer client.Disconnect(context.Background())
 
-	db := client.Database("shonen-jump")
-	mangaCollection := db.Collection("manga")
+	// db := client.Database("shonen-jump")
+	// mangaCollection := db.Collection("manga")
 
-	results, err := web.ScrapeRecentChapters()
-	if err != nil {
-		panic(err)
-	}
+	// mangaDocs := []interface{}{}
+	// for _, manga := range *results {
+	// 	mangaDocs = append(mangaDocs, manga)
+	// }
 
-	mangaDocs := []interface{}{}
-	for _, manga := range *results {
-		mangaDocs = append(mangaDocs, manga)
-	}
+	// insert, err := mangaCollection.InsertMany(context.TODO(), mangaDocs)
+	// if err != nil {
+	// 	panic(err)
+	// }
 
-	insert, err := mangaCollection.InsertMany(context.TODO(), mangaDocs)
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println(insert)
+	// if insert != nil {
+	// 	fmt.Println("Inserted multiple documents")
+	// }
 }
--- web/scrape.go
+++ web/scrape.go
@@ -10,9 +10,19 @@ import (
 
 type RecentManga struct {
 	Title         string `json:"title" bson:"title,omitempts"`
-	Link          string `json:"link" bson:"link,omitempts"`
+	Handle        string `json:"handle" bson:"handle,omitempts"`
 	RecentChapter string `json:"recent_chapters" bson:"recent_chapter,omitempts"`
 	RecentLink    string `json:"recent_link" bson:"recent_link,omitempts"`
+}
+
+type Manga struct {
+	Title         string   `json:"title" bson:"title,omitempts"`
+	Handle        string   `json:"handle" bson:"handle,omitempts"`
+	Description   string   `json:"description" bson:"description,omitempts"`
+	Author        string   `json:"author" bson:"author,omitempts"`
+	Image         string   `json:"hero_image" bson:"image,omitempts"`
+	LatestRelease string   `json:"latest_release" bson:"latest_release,omitempts"`
+	Recommended   []string `json:"recommended" bson:"recommended,omitempts"`
 }
 
 func ScrapeRecentChapters() (*[]RecentManga, error) {
@@ -31,15 +41,55 @@ func ScrapeRecentChapters() (*[]RecentManga, error) {
 	})
 
 	c.OnHTML(".o_sortable", func(e *colly.HTMLElement) {
-		recent := strings.Split(e.ChildText("span"), "  ")[0]
+		recent := strings.Split(e.ChildText("span"), "\n")[0]
 		manga := RecentManga{
 			Title:         e.ChildText("div.type-center"),
-			Link:          e.ChildAttr("a", "href"),
+			Handle:        strings.Replace(e.ChildAttr("a", "href"), "/shonenjump/chapters/", "", -1),
 			RecentChapter: strings.Replace(recent, "Latest: ", "", -1),
 			RecentLink:    e.ChildAttr("a.o_inner-link", "href"),
 		}
 		results = append(results, manga)
 	})
 	c.Visit(recentURL)
+	return &results, nil
+}
+
+func ScrapeSeries(handle string) (*Manga, error) {
+	seriesURL := "https://www.viz.com/shonenjump/chapters/" + handle
+	results := Manga{}
+
+	c := colly.NewCollector()
+
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL)
+	})
+
+	c.OnError(func(_ *colly.Response, err error) {
+		s := fmt.Sprintf("Something went wrong getting data on %s: ", handle)
+		log.Panic(s, err)
+	})
+
+	c.OnHTML("body", func(e *colly.HTMLElement) {
+		recommended := []string{}
+		title := e.ChildText("h2.type-lg")
+		description := e.ChildText("div.line-solid.type-md")
+		author := e.ChildText("span.disp-bl--bm")
+		image := e.ChildAttr("img.o_hero-media", "src")
+		latest_release := strings.Split(e.ChildText("div.flex-width-2.type-bs.type-sm--sm.style-italic > table > tbody > tr > td"), ", ")[0]
+		e.ForEach("a.o_property-link", func(_ int, el *colly.HTMLElement) {
+			handle := strings.Replace(el.Attr("href"), "/shonenjump/chapters/", "", -1)
+			recommended = append(recommended, handle)
+		})
+		manga := Manga{
+			Title:         title,
+			Description:   description,
+			Author:        author,
+			Image:         image,
+			LatestRelease: latest_release,
+			Recommended:   recommended,
+		}
+		results = manga
+	})
+	c.Visit(seriesURL)
 	return &results, nil
 }
