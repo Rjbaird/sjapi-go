39130013241bf083be930a388258e6199c911cc1
Rjbaird <rjbaird09@gmail.com>
2023-08-31 18:38:36 -0500

    fix font and add date

--- controllers/html.go
+++ controllers/html.go
@@ -1,19 +1,30 @@
 package controllers
 
 import (
+	"fmt"
+	"time"
+
 	"github.com/bairrya/sjapi/db"
 	"github.com/gofiber/fiber/v2"
 )
 
 func MainPage(c *fiber.Ctx) error {
+	now := time.Now()
+	today := fmt.Sprintf("%s, %s %v, %v", now.Weekday(), now.Month(), now.Day(), now.Year())
 	manga, err := db.FindRecentManga()
 	if err != nil {
 		return c.Status(500).JSON(fiber.Map{
 			"error": "Internal Server Error",
 		})
 	}
+
+	d := map[string]interface{}{}
+
+	d["Manga"] = manga
+	d["Meta"] = today
+
 	return c.Render("manga", fiber.Map{
-		"Manga": manga,
+		"Data": d,
 	})
 }
 
